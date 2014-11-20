f34cd34123dda2612697b419d79f5d9d2e55bef7
Daniel Husar <dano.husar@gmail.com>
2014-11-20 22:52:15 +0100

    Update filter.js

--- app/scripts/filter.js
+++ app/scripts/filter.js
@@ -1,4 +1,14 @@
 (function () {
   'use strict';
   
+  var settings = {
+    'images': ['jpeg', 'jpg', 'png', 'gif'],
+    'videos': ['mp4', 'webm'],
+    'html': ['html', 'erb']
+    'styles': ['css', 'sass', 'scss', 'less', 'styl'],
+    'javascript': ['js', 'coffee'],
+    'ruby': ['rb', 'ru'],
+    'python' : ['py']
+  };
+  
 })();
