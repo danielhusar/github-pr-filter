91dfcb0e2f6ab4b7a3e2d9ab01d9791852c6ff19
Daniel Husar <dano.husar@gmail.com>
2014-11-21 20:17:23 +0100

    Update filter.js

--- app/scripts/filter.js
+++ app/scripts/filter.js
@@ -11,4 +11,24 @@
     'python' : ['py']
   };
   
+  function $ (selector, context) {
+    context = context ? context : document;
+    this.items = context.querySelectorAll(selector);
+    return this;
+  }
+  
+  $.addClass = function (cl) {
+    this.items.each(function () {
+      this.classList.add(cl);
+    });
+    return this;
+  };
+  
+  $.removeClass = function (cl) {
+    this.items.each(function () {
+      this.classList.remove(cl);
+    });
+    return this;
+  };
+  
 })();
