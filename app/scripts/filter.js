f49074b3f4996f9c3c2223f709fa3bc9d0b3fd21
Daniel Husar <dano.husar@gmail.com>
2014-12-04 12:09:57 +0000

    Update filter.js

--- app/scripts/filter.js
+++ app/scripts/filter.js
@@ -10,26 +10,25 @@
     'ruby': ['rb', 'ru'],
     'python' : ['py']
   };
-  
-  function $ (selector, context) {
-    context = context ? context : document;
-    this.items = context.querySelectorAll(selector);
-    
-    this.addClass = function (cl) {
-      this.items.each(function () {
-        this.classList.add(cl);
-      });
-      return this;
-    };
-    
-    this.remveClass = function (cl) {
-      this.items.each(function () {
-        this.classList.remove(cl);
-      });
-      return this;
-    };
-    
+
+  function jQuery(selector) {
+    this.el = [].slice.call(document.querySelectorAll(selector));
+    this.length = this.el.length;
     return this;
   }
+  
+  jQuery.prototype = {
+    each: function(fn) {
+      this.el.forEach(function (element, index) {
+        fn.apply(element, [element, index]);
+      });
+      return this;
+    }
+  };
+  
+  function $(selector) {
+    return new jQuery(selector);
+  }
+
   
 })();
