4986f0dc1ff82ae6669cc33365185089f289a61e
Daniel Husar <dano.husar@gmail.com>
2014-12-23 15:49:57 +0000

    generate nav

--- init.js
+++ init.js
@@ -4,6 +4,30 @@
   var $ = window.jQuery;
 
   function init () {
+
+    if ($('#filter-nav').length > 0) {
+      return;
+    }
+
+    var nav = '<div class="subnav-links" id="filter-nav" style="overflow: hidden; width: 100%; margin-top: 20px; text-transform: uppercase; letter-spacing: 1px;">';
+    nav += '<a href="#" class="subnav-item selected" data-all="true">All</a>';
+
+    var items = $('.info .js-selectable-text')
+                  .map(function () {
+                    return /[^.]+$/.exec($(this).attr('title'));
+                  })
+                  .splice(0)
+                  .filter(function (e, i, arr) {
+                    return arr.lastIndexOf(e) === i;
+                  });
+
+    items.forEach(function (el) {
+      nav += '<a href="#" class="subnav-item">' + el + '</a>';
+    });
+    nav += '</div>';
+
+    $('#toc').append(nav);
+
   }
 
   init();
